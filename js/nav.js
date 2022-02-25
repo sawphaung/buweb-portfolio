dd24d8248ef8f9c9d1a15001774ed02b2bb1b3ba
Saw Phaung <saw@77creative.io>
2022-02-24 20:11:48 -0600

    update link

--- js/nav.js
+++ js/nav.js
@@ -11,12 +11,12 @@ class Nav extends HTMLElement {
            <header class="nav-header">
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/Projects.html">Projects</a></li>
-                <li><a href="/Database.html">Database Diagrams</a></li>
-                <li><a href="/API.html">API Unit Tests</a></li>
-                <li><a href="/About.html">About</a></li>
-                <li><a href="/Resume.html">Resume</a></li>
+                <li><a href="./">Home</a></li>
+                <li><a href="./Projects.html">Projects</a></li>
+                <li><a href="./Database.html">Database Diagrams</a></li>
+                <li><a href="./API.html">API Unit Tests</a></li>
+                <li><a href="./About.html">About</a></li>
+                <li><a href="./Resume.html">Resume</a></li>
             </ul>
         </nav>
     </header>
